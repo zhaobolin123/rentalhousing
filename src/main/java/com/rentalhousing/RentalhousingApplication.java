1c82f718c3956ca6ff4e17ecee8ef1353b3c3ed4
bolin.zhao <648923613@qq.com>
2020-05-08 15:29:45 +0800

    解决跨域问题

--- src/main/java/com/rentalhousing/RentalhousingApplication.java
+++ src/main/java/com/rentalhousing/RentalhousingApplication.java
@@ -1,6 +1,5 @@
 package com.rentalhousing;
 
-import javafx.application.Application;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/rentalhousing/config/CrossFilter.java
+++ src/main/java/com/rentalhousing/config/CrossFilter.java
@@ -3,8 +3,11 @@ package com.rentalhousing.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -15,6 +18,8 @@ import java.io.IOException;
  * @Param：
  * @return ：
  */
+@Component
+@WebFilter(urlPatterns = "/*",filterName = "CrossFilter")
 public class CrossFilter implements Filter {
     private static Logger logger = LoggerFactory.getLogger(CrossFilter.class);
 
@@ -24,11 +29,17 @@ public class CrossFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+
+        response.setHeader("Access-Control-Allow-Origin", "*"); //解决跨域访问报错
+        response.setHeader("Access-Control-Allow-Credentials","true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600"); //设置过期时间
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, client_id, uuid, Authorization");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // 支持HTTP 1.1.
+        response.setHeader("Pragma", "no-cache"); // 支持HTTP 1.0. response.setHeader("Expires", "0");
+
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
