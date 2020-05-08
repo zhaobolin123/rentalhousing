74690ffdc115268665b07793260b363f95c48284
bolin.zhao <648923613@qq.com>
2020-05-08 11:29:32 +0800

    修改打包方式

--- src/main/java/com/rentalhousing/RentalhousingApplication.java
+++ src/main/java/com/rentalhousing/RentalhousingApplication.java
@@ -1,15 +1,21 @@
 package com.rentalhousing;
 
+import javafx.application.Application;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan("com.rentalhousing.mapper")
 @SpringBootApplication
-public class RentalhousingApplication {
+public class RentalhousingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RentalhousingApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RentalhousingApplication.class);
+    }
 }
